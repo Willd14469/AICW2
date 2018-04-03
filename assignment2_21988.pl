8f6ac3df4dfc14aee5fb9555e2e1c5081775675c
bpatty95 <pb15724@my.bristol.ac.uk>
2018-04-03 17:48:18 +0100

    part 1 and part 2 working, up to 50%

--- assignment2_21988.pl
+++ assignment2_21988.pl
@@ -1,31 +1,76 @@
 candidate_number(21988).
 
-solve_task(Task,Cost):-
+solve_task(Task,Cost) :-
+  part_module(1) -> solve_task_1(Task, Cost).
+
+
+solve_task_1(Task,Cost):-
   my_agent(Agent),
-  query_world( agent_current_position, [Agent,P] ),
-  solve_task_bt(Task,[c(0,P),P],0,R,Cost,_NewPos),!,  % prune choice point for efficiency
-  reverse(R,[_Init|Path]),
+  query_world( agent_current_position, [Agent,Position] ),
+  calc_f_value(Task, Position, 0, F),
+  solve_task_a_star(Task, [[c(FCost, 0, Position), Position]], ReversPath, Cost, _),!, % prune choice point for efficiency
+  reverse(ReversPath,[_Init|Path]),
   query_world( agent_do_moves, [Agent,Path] ).
+
+%% A* search 
+solve_task_a_star(Task,[Current|_],ReversPath,[cost(Cost),depth(Depth)],NewPos) :-
+  achieved(Task,Current,ReversPath,Cost,NewPos),
+  length(ReversPath,Depth).
+solve_task_a_star(Task,Agenda,ReversPath,Cost,NewPos) :-
+  Agenda = [Current|AgendaTail], % Get current node
+  find_children(Task, Current, Children),
+  insert_many_in_agenda(Children, AgendaTail, NewAgenda),
+  solve_task_a_star(Task, NewAgenda, ReversPath, Cost, NewPos).
+
+insert_many_in_agenda([], Agenda, Agenda).
+insert_many_in_agenda([Child|Children], Agenda, NewAgenda) :-
+  insert_agenda(Child, Agenda, TempAgenda), % TempAgenda is Agenda without Child
+  insert_many_in_agenda(Children, TempAgenda, NewAgenda).
+
+insert_agenda(Node,Agenda,Agenda) :- repeat_node(Node,Agenda), ! .
+insert_agenda(Node,[A|R],[Node,A|R]) :- cheaper_cost(Node,A), ! .
+insert_agenda(Node,[A|R],[A|S]) :- insert_agenda(Node,R,S), !.
+insert_agenda(Node,[],[Node]).
+
+
+repeat_node([c(_, _, Position)|_], [[c(_, _, Position)|_]|_]).
+cheaper_cost([c(FCost1, _, _)|_], [c(FCost2, _, _)|_]) :- FCost1 <  FCost2.
+
+find_children(Task, Node, Children) :-
+  Node = [c(_, G, NodePos)|Path],
+  (bagof([c(ChildF, ChildG, ChildPos), ChildPos|Path],
+    ( search(NodePos, ChildPos, ChildPos, C),
+      \+ memberchk(ChildPos, Path), % Don't unclude if already visited
+      ChildG is G + C,
+      calc_f_value(Task, ChildPos, ChildG, ChildF)
+    ), Children); Children = []). % Fill children or empty
+
+calc_f_value(find(_), _, G, F) :-
+  F is G.
+calc_f_value(go(TargetPos), Position, G, F) :-
+  map_distance(Position, TargetPos, H),
+  F is G + H.
+
 
 %%%%%%%%%% Useful predicates %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% backtracking depth-first search, needs to be changed to agenda-based A*
 solve_task_bt(Task,Current,Depth,RPath,[cost(Cost),depth(Depth)],NewPos) :-
   achieved(Task,Current,RPath,Cost,NewPos).
 solve_task_bt(Task,Current,D,RR,Cost,NewPos) :-
-  Current = [c(F,P)|RPath],
+  Current = [c(_,F,P)|RPath],
   search(P,P1,R,C),
   \+ memberchk(R,RPath),  % check we have not been here already
   D1 is D+1,
   F1 is F+C,
-  solve_task_bt(Task,[c(F1,P1),R|RPath],D1,RR,Cost,NewPos).  % backtrack search
+  solve_task_bt(Task,[c(_,F1,P1),R|RPath],D1,RR,Cost,NewPos).  % backtrack search
 
 achieved(go(Exit),Current,RPath,Cost,NewPos) :-
-  Current = [c(Cost,NewPos)|RPath],
+  Current = [c(_,Cost,NewPos)|RPath],
   ( Exit=none -> true
   ; otherwise -> RPath = [Exit|_]
   ).
 achieved(find(O),Current,RPath,Cost,NewPos) :-
-  Current = [c(Cost,NewPos)|RPath],
+  Current = [c(_,Cost,NewPos)|RPath],
   ( O=none    -> true
   ; otherwise -> RPath = [Last|_],map_adjacent(Last,_,O)
   ).
--- assignment2_wp_21988.pl
+++ assignment2_wp_21988.pl
@@ -1,14 +1,25 @@
 % candidate_number(12345).
 
-% Find hidden identity by repeatedly calling agent_ask_oracle(oscar,o(1),link,L)
+%% % Find hidden identity by repeatedly calling agent_ask_oracle(oscar,o(1),link,L)
 % find_identity(-A)
+
 find_identity(A):-
-  (part_module(2)   -> find_identity_2(A)
-  ; otherwise -> find_identity_o(A)
+  findall(PotentialActor, actor(PotentialActor), PotentialActors),
+  find_identity_1(A, PotentialActors),!.
+
+find_identity_1(Actor, [Actor]). % Return when we have one answer
+find_identity_1(Actor, PotentialActors) :-
+  agent_ask_oracle(oscar, o(1), link, Link),
+  find_actors_link(Link, PotentialActors, [], ReducedPotentials),
+  find_identity_1(Actor, ReducedPotentials).
+
+find_actors_link(_, [], ActorsLink, ActorsLink). % Once list exhausted
+find_actors_link(Link, [Actor|Actors], UpdatedLink, ActorsLink) :-
+  wp(Actor, WT),
+  findall(X, wt_link(WT, X), Links),
+  ( memberchk(Link, Links) -> find_actors_link(Link, Actors, [Actor|UpdatedLink], ActorsLink)
+  ; otherwise              -> find_actors_link(Link, Actors, UpdatedLink, ActorsLink)
   ).
 
-find_identity_2(A):-
-  A='Not yet implemented'.
 
-find_identity_o(A):-
-  A='Not yet implemented'.
+
